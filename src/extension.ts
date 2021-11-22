5f71b50a4bb4f13eea70a34879fedeecf8fdd292
liguanliang1 <liguanliang1@sensetime.com>
2021-11-22 16:08:52 +0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -101,28 +101,6 @@ function init() {
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "a-stock-for-money" is now active!',
-  );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    'a-stock-for-money.show',
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        'show stock from a-stock-for-money!',
-      );
-    },
-  );
-
-  context.subscriptions.push(disposable);
-
   init();
   context.subscriptions.push(
     vscode.workspace.onDidChangeConfiguration(handleConfigChange),
